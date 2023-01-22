7af36111d17f4f43f2f52fe92ce55f6e0893f3ee
sophiapeach7 <123259297+sophiapeach7@users.noreply.github.com>
2023-01-21 20:24:27 -0500

    Same code but uses parallel computing for the main for loop. !!READ COMMENT!!
    !!Requires parallel computing toolkit!!

--- IJMrunMAT.m
+++ IJMrunMAT.m
@@ -5,7 +5,7 @@ if cleanup == "YES"
     og_dir = pwd;
     cd(datadirectory)
     files = dir('*.txt');
-    for i = 1:length(files)
+    parfor i = 1:length(files)
         [empty,name] = fileparts(files(i).name);
         img = readmatrix(files(i).name);
         for n = 1:2040
@@ -20,6 +20,6 @@ if cleanup == "YES"
                 end
             end
         end
-        writematrix(img,files(i).name)
+        writematrix(img,files(i).name);
     end
 end
