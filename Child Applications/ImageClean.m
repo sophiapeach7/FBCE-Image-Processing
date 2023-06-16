4807a8b44b088856b433df716c18ddf36e645a77
sophiapeach7 <123259297+sophiapeach7@users.noreply.github.com>
2023-06-16 19:40:59 -0400

    Accelerated code by checking first whether there are any shadows in the image as a whole

--- Child Applications/ImageClean.m
+++ Child Applications/ImageClean.m
@@ -3,7 +3,7 @@ datadirectory = 'C:\Users\Sophia\Documents\GitHub\FBCE_ImageProcessing\_dir_inte
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 D = parallel.pool.DataQueue;
-h = waitbar(0, 'Please wait ...');
+h = waitbar(0, 'Program initiaiting...');
 afterEach(D, @nUpdateWaitbar);
 N = 1;
 
@@ -13,19 +13,29 @@ nfiles = length(files);
 parfor i = 1:nfiles
     [empty,name] = fileparts(files(i).name);
     img = readmatrix(files(i).name);
-    for n = 1:2040
-        for m = 15:30
-            if img(m,n) == 255
-                if img(33,n) == 255
-                    break
-                else
-                    img(1:33,n) = 0;
-                    break
+    if sum(img(15:30,1:2040),"all") > 3*255
+        ndiv = 4;
+        range = 2040/ndiv;
+        for q = 1:ndiv
+            nstart = 1+(q-1)*range;
+            nstop = q*range;
+            for n = nstart:nstop
+                if sum(img(15:30,nstart:nstop),"all") > 0
+                    for m = 15:30
+                        if img(m,n) == 255
+                            if img(33,n) == 255
+                                break
+                            else
+                                img(1:33,n) = 0;
+                                break
+                            end
+                        end
+                    end
                 end
             end
         end
+        writematrix(img,files(i).name)
     end
-    writematrix(img,files(i).name)
     send(D, [i nfiles]);
 end
 
@@ -40,7 +50,7 @@ N = evalin("base","N");
 h = findall(0, 'type', 'figure', 'Tag', 'TMWWaitbar');
 p = N/input(2);
 
-waitbar(p, h);
+waitbar(p, h, "Cleaning...");
 assignin("base","N",N+1);
 
 end
