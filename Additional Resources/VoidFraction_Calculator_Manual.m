8069b9a089bd79ae62c9398cc018ff39d7c130e1
sophiapeach7 <123259297+sophiapeach7@users.noreply.github.com>
2023-07-15 20:50:20 -0400

    Added new ImageJ macro for separate background image breakdown to be used with Individual void fraction calculator

--- Additional Resources/VoidFraction_Calculator_Manual.m
+++ Additional Resources/VoidFraction_Calculator_Manual.m
@@ -1,80 +1,84 @@
+%% LOADING SETTINGS
+clear;
+clc;
+close all force;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-loaddata = "NO";
-createavrgmatrix = "NO";
-savematrix = "NO";
-plotdata = "NO";
-n1p = 0; %number of single phase datum points at the BEGINNING
-plot_starting_DP = 1; %NOT same as avrgmatrix indexation, refer to test matrices for correct DP value
-save_starting_DP = 1;
+Temp_Dir = "C:/Users/Sophia/Documents/GitHub/FBCE_ImageProcessing";
+%Settings = readmatrix(Temp_Dir+"/_intermediate_data_transfer_.csv","OutputType","string","Range",1);
+%delete(Temp_Dir+"/_intermediate_data_transfer_.csv");
+bck_gv = readmatrix(Temp_Dir+"/Background_GrayValues.csv");
+delete(Temp_Dir+"/Background_GrayValues.csv");
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-datadirectory = 'Full directory of ImageJ Profile Plot data';
-savedirectory = 'Full directory where to save output matrix';
-savefileNAME = 'Output matrix name';
-plottitle = 'Experiment name';
+n1p = str2double(Settings(2,1)); %number of single phase datum points at the BEGINNING
+plot_starting_DP = n1p+1; %NOT same as avrgmatrix indexation, refer to test matrices for correct DP value
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+datadirectory = Settings(2,2); %Full directory of ImageJ Profile Plot data
+savedirectory = Settings(2,3); %Full directory where to save output matrix
+save_csv_fileNAME = Settings(2,4); %Output matrix name
+save_plot_fileNAME = Settings(2,5); %Outpot plot files name
+plottitle = Settings(2,6); %Experiment name
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if loaddata == "YES"
-    cd(datadirectory)
-    files = dir('*.csv');
-    for i = 1:length(files)
-        load(files(i).name, '-ascii');
+%% LOADING DATA
+cd(datadirectory)
+files = dir('*.csv');
+wbar = waitbar(0,"Loading data...");
+for i = 1:length(files)
+    load(files(i).name, '-ascii');
+    waitbar(i/length(files),wbar);
+end
+close(wbar);
+nfiles = i;
+
+avrgmatrix = zeros(2040,nfiles+1+n1p);
+step = linspace(0,1,2040);
+step = transpose(step);
+avrgmatrix(:,1) = step;
+
+%% CREATING ZEROS ARRAY FOR SINGLE-PHASE DATUM POINTS
+if n1p > 0
+    array0 = zeros(2040,1);
+    for p = 2:n1p+2
+        avrgmatrix(:,p) = array0;
     end
-    nfiles = i;
 end
 
-if createavrgmatrix == "YES"
-    if exist('nfiles') == 0
-        error("Data Files are NOT imported. Use loaddata option.")
-    end
+%% PROCESSING DATA
 
-    avrgmatrix = zeros(2040,nfiles+1+n1p);
-    step = linspace(0,1,2040);
-    step = transpose(step);
-    avrgmatrix(:,1) = step;
+bck_gv = bck_gv*164/255;
 
-    if n1p > 0
-        array0 = zeros(2040,1);
-        for p = 2:n1p+2
-            avrgmatrix(:,p) = array0;
-        end
-    end
-
-    for i = 2+n1p:nfiles+1+n1p
-        [empty,name] = fileparts(files(i-1-n1p).name);
-        xname = append('X',name);
-        DP = eval(xname);
-        n = eval(name);
-        navrg = vecavrg(DP);
-        avrgmatrix(:,n+1) = navrg;
-    end
-
-end 
-
-if savematrix == "YES"
-    if exist('avrgmatrix') == 0
-        error("Output matrix does not exist.")
-    end
-    cd(savedirectory)
-    saveavrgmatrix = [avrgmatrix(:,1) avrgmatrix(:,save_starting_DP:end)];
-    writematrix(saveavrgmatrix,savefileNAME)
+for i = 2+n1p:nfiles+1+n1p
+    [empty,name] = fileparts(files(i-1-n1p).name);
+    xname = append('X',name);
+    DP = eval(xname);
+    n = eval(name);
+    navrg = vecavrg(DP,bck_gv);
+    avrgmatrix(:,n+1) = navrg;
 end
 
-if plotdata == "YES"
-    if exist('avrgmatrix') == 0
-        error("Output matrix does not exist.")
-    end
-    columns = size(avrgmatrix,2);
-    for p=1+plot_starting_DP:columns
-        plot(avrgmatrix(:,1),avrgmatrix(:,p))
-        text(1+0.005,avrgmatrix(end,p),string(p-1),'Color','red','FontSize',10)
-        hold on
-    end
-    title("Void Fraction Plots for Each Datum Point ("+plottitle+")")
-    xlabel("Length of the Channel, x/L")
-    ylabel("Void Fraction")
-end
+%% SAVING RESULTS
+writematrix(avrgmatrix,savedirectory+save_csv_fileNAME+".csv");
 
-function avrg = vecavrg(n)
+%% PLOTTING DATA
+columns = size(avrgmatrix,2);
+VFplot = figure("Name","Void Fraction Plots","Units","normalized","Position",[0.05 0.05 0.9 0.8]);
+VFplot.WindowState = "maximized";
+for p=1+plot_starting_DP:columns
+    plot(avrgmatrix(:,1),avrgmatrix(:,p))
+    text(1+0.005,avrgmatrix(end,p),string(p-1),'Color','red','FontSize',10)
+    hold on
+end
+title(plottitle)
+xlabel("Length of the Channel, x/L")
+ylabel("Void Fraction")
+ylim([0 1]);
+xlim([0 1]);
+savefig(VFplot,savedirectory+save_plot_fileNAME+".fig");
+saveas(VFplot,savedirectory+save_plot_fileNAME+".png");
+
+%% FUNCTIONS
+function avrg = vecavrg(n,bck_gv)
 nrows = size(n,1);
 avrg = zeros(nrows,1);
 
@@ -83,6 +87,18 @@ for k=1:nrows
     avrg(k) = mean(row);
 end
 
-avrg = avrg*164/255/103;
+avrg = avrg*164/255;
+avrg = avrg./bck_gv;
 
+end
+
+
+function result = datainput
+f = figure("Name","EXPERIMENT INFORMATION");
+f.Position(3:4) = [500 300];
+W = f.Position(3);
+H = f.Position(4);
+uicontrol(f,"Style","text","String","Please input experiment information","Position",[20 H-30 W-5 20],"HorizontalAlignment","left","FontSize",10)
+uicontrol(f,"Style","text","String","Number of single-phase datum points at the beginning:","Position",[20 H-70 W-5 20],"HorizontalAlignment","left","FontSize",10)
+SinglePhaseDP = uicontrol(f,"Style","edit","Position",[350,H-70,50,20],"HorizontalAlignment","left","FontSize",10);
 end
